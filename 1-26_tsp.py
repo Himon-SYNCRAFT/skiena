72ed4de2883a14b5bf8dbca5d980ab3b346c2dfd
Himon <d.zawlocki@eltrox.pl>
2016-01-04 21:40:37 +0100

    Working version of 1-26 closest pair
    First working(and messy) version of closest pair TSP heuristic.
    Needs additional testing and refactoring.

--- 1-26_tsp.py
+++ 1-26_tsp.py
@@ -27,41 +27,59 @@ def nearest_neighbor(edges, start_point=0):
     return path
 
 
-def closest_pair(edges):
-    visited = set()
-    path = []
+def closest_pair(edges):    
     chains = []
     min_key = min(edges, key=edges.get)
-    chains.append([min_key])
+    chains.append(list(min_key))
     del edges[min_key]
 
     while edges:
         min_key = min(edges, key=edges.get)
-        connection_point = None
+        current_edge = list(min_key)
+        connected_points = []
+        chains_to_remove = []
+        for chain_index in range(len(chains)):
+            chain = chains[chain_index]
+            if current_edge[0] == chain[len(chain) - 1]:
+                connected_points.append(current_edge[0])
+                chains_to_remove.append(chain_index)
+                chain_copy = chain.copy()
+                chain_copy.extend(current_edge)
+                current_edge = chain_copy
+            elif current_edge[0] == chain[0]:
+                connected_points.append(current_edge[0])
+                chains_to_remove.append(chain_index)
+                current_edge.reverse()
+                current_edge.extend(chain)
+            elif current_edge[1] == chain[len(chain) - 1]:
+                connected_points.append(current_edge[1])
+                chains_to_remove.append(chain_index)
+                chain_copy = chain.copy()
+                current_edge.reverse()
+                chain_copy.extend(current_edge)
+                current_edge = chain_copy
+            elif current_edge[1] == chain[0]:
+                connected_points.append(current_edge[1])
+                chains_to_remove.append(chain_index)
+                current_edge.extend(chain)
 
-        for chain in chains:
-            if min_key[0] in chain[0]:
-                chain.insert(0, min_key)
-                connection_point = min_key[0]
-            elif min_key[0] in chain[len(chain) - 1]:
-                chain.append(min_key)
-                connection_point = min_key[0]
-            elif min_key[1] in chain[0]:
-                chain.insert(0, min_key)
-                connection_point = min_key[1]
-            elif min_key[1] in chain[len(chain) - 1]:
-                chain.append(min_key)
-                connection_point = min_key[1]
+            print(edges)
 
-            #print('chain', chain)
-        if connection_point is None:
-            chains.append([min_key])
+        if not connected_points:
             del edges[min_key]
+        else:
+            for p in connected_points:
+                for e in list(edges.keys()):
+                    if p in e:
+                        print(edges)
+                        del edges[e]
 
-        for edge in list(edges.keys()):
-            if connection_point in edge:
-                del edges[edge]
-        print('edges', edges.keys())
+            chains_to_remove.sort()
+            chains_to_remove.reverse()
+            for i in chains_to_remove:
+                del chains[i]
+
+        chains.append(current_edge)
 
     return chains
 
@@ -82,4 +100,4 @@ if __name__ == "__main__":
     }
 
     print(closest_pair(edges))
-    unittest.main()
+    # unittest.main()
